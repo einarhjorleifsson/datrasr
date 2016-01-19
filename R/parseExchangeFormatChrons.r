d2201fdf81bf66382acd33e6c37a1eec15e4d63d
Einar Hjorleifsson <einar.hjorleifsson@gmail.com>
2016-01-19 08:44:07 +0000

    runs practical01

--- R/parseExchangeFormatChrons.r
+++ R/parseExchangeFormatChrons.r
@@ -20,13 +20,13 @@ for(i in 1:length(fnames)){
 cnts<-count.fields(fnames[i],sep=",")
 chrons<-readLines(fnames[i])
 chrons<-cbind(chrons,cnts)
-chrons<-chrons[chrons[,2]=="59",][,-2]
+chrons<-chrons[chrons[,2]=="61",][,-2]
 print(length(chrons))
 if(i == 1){hds<-chrons[1]}
 out<-c(out,chrons[-1])
 }
 
-chrons <- data.frame(matrix(unlist(strsplit(out,",")),byrow=T,ncol=59))
+chrons <- data.frame(matrix(unlist(strsplit(out,",")),byrow=T,ncol=61))
 
 dimnames(chrons)[[2]] <- tolower(strsplit(hds,",")[[1]])
 
